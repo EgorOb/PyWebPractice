7f80bb0d02d179f9034f4150b60f8ea2ca2607b9
Egor <egorobidin@inbox.ru>
2024-04-02 15:03:20 +0300

    update

--- apps/db_train_alternative/views.py
+++ apps/db_train_alternative/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from django.views import View
+from .models import Author
+
+
+class AuthorREST(View):
+    def get(self, request, id=None):
+
+        if id is None:
+            for author in Author.objects.all():
+                pass
+        else:
+            data = get_object_or_404(Author, id=id)
+
+
+        return JsonResponse(list(data.values()), safe=False, json_dumps_params={"ensure_ascii": False,
+                                                       "indent": 4})
+
 
 # Create your views here.
